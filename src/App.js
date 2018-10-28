5404bc14914cacbbec96b64bd3edc65769c23e10
YixiaoTang <963726538@qq.com>
2018-10-28 16:31:33 -0700

    Fetch Constants send POST Request

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import {TopBar} from './TopBar';
 import './App.css';
 import {Register} from './Registration';
-
+import {Main} from'./Main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <TopBar/>
-        <Register/>
+        <Main/>
       </div>
     );
   }
