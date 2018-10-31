1a46090701583855bb3942005191d8062cec4cb3
YixiaoTang <963726538@qq.com>
2018-10-30 21:01:23 -0700

    Links

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,18 @@
 import  React from 'react';
 import {Register} from './Registration';
+import { Switch, Route } from 'react-router-dom'
+import {Login} from "./Login";
+
 export class Main extends React.Component{
     render(){
         return(
             <div className='main'>
-                <Register/>
+                <Switch>
+                    <Route exact path="/" component={Login}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/register" component={Register}/>
+                    <Route component={Login}/>
+                </Switch>
             </div>
         )
     }
--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,7 +1,7 @@
-
 import React from 'react';
 import { Form, Input, Button, message} from 'antd';
 import {API_ROOT} from "../constants"
+import {Link} from 'react-router-dom';
 
 const FormItem = Form.Item;
 
@@ -128,6 +128,9 @@ class RegistrationForm extends React.Component {
                 </FormItem>
                 <FormItem {...tailFormItemLayout}>
                     <Button type="primary" htmlType="submit">Register</Button>
+                    <p>
+                        I already have an account, go back to <Link to="/login"> login</Link>
+                    </p>
                 </FormItem>
             </Form>
         );
